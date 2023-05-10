8923846dfd4a6583440e070da768d360a0aacbb7
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2023-05-10 17:07:21 -0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+const WINNER_COMPS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  ];
+
+const GRID = Array.from(Array(9).keys());
+
+function App() {
+  return (
+    <main>
+      {GRID.map((i) => (
+        <div key={i} >
+          {""}
+        </div>
+      ))}
+    </main>
+  );
+}
+
+export default App;
